537c9c72495007d704acc68b63f47eba2264d8f8
suyeon <rlatndus9911@naver.com>
2022-01-07 23:28:26 +0900

    해빗 연습 프로젝트 생성

---
+++ Havit_Practice/Havit_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Havit_Practice
+//
+//  Created by 김수연 on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
