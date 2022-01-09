e020a0413bb5503dfc848504ea1092c8a417105e
suyeon <rlatndus9911@naver.com>
2022-01-10 02:40:18 +0900

    더미데이터 변경

--- Havit_Practice/Havit_Practice/CategoryListModel.swift
+++ Havit_Practice/Havit_Practice/CategoryListModel.swift
@@ -17,17 +17,17 @@ struct CategoryListModel {
 
     init() {
         self.data = [
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클"),
-            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클")
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클1"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클2"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클3"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클4"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클5"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클6"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클7"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클8"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클9"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클10"),
+            Category(categoryImageName: "category_icon", categoryTitle: "UX/UI 아티클11")
         ]
     }
 
